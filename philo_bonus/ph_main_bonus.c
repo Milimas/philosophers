7f0bd0e55d9d774bdb32e196b63c1f9ae67a89b1
Amine Beihaqi <abeihaqi@e2r10p2.1337.ma>
2023-02-27 02:14:54 +0100

    added destroy functions to main

--- philo_bonus/ph_main_bonus.c
+++ philo_bonus/ph_main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: abeihaqi <abeihaqi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 02:05:36 by aminebeihaq       #+#    #+#             */
-/*   Updated: 2023/02/27 00:05:34 by abeihaqi         ###   ########.fr       */
+/*   Updated: 2023/02/27 02:04:23 by abeihaqi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,7 @@ int	main(int argc, char **argv)
 	init_philos(argc, argv, &philo, &rules);
 	start_philos(philo);
 	wait_philos(philo, &rules);
+	destroy_philos(philo);
+	destroy_rules(&rules);
 	return (0);
 }
